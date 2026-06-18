5f8a8aa00234a55b91309e57fe27e2864345c0d6
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 14:56:33 -0500

    docs: add starter file for lesson 4

---
+++ starter-files/lesson-4-methods-scanner-and-oop/Main.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.util.Scanner; 
+
+class Pet {
+  //Define atributos para tu clase.
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Inicializa Scanner escribiendo esta línea:
+        // Scanner scanner = new Scanner(System.in);
+        
+        // Crea tu clase, por ejemplo:
+        // Pet myPet = new Pet();
+        
+        // Configura el teclado para que permita escribir y asignar valores:
+        // System.out.print("¿Cómo se llama tu compañero? ");
+        // myPet.name = scanner.nextLine();
+        
+        // Cierra el Scanner como buena práctica.
+        // scanner.close();
+    }
+}
