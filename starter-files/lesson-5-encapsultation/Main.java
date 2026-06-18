760165108ef9ed3b0795aec5e56c36f14284c47d
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 15:00:35 -0500

    docs: add starter file for lesson 5

---
+++ starter-files/lesson-5-encapsultation/Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+class Example {
+    //Define atributos y métodos. Haz los atributos privados, y no olvides colocar getters y setters.
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+       //Inicializa el scanner, escribe variables, valida datos mediante métodos encapsulados, e imprime la información.
+    }
+}
