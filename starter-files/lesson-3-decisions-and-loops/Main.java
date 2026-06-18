f43d3f36578784b9790001870c83b33839e6d9f1
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 14:52:12 -0500

    docs: moved starter file for lesson 3 to folder

---
+++ starter-files/lesson-3-decisions-and-loops/Main.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        int edad = 15;
+        System.out.println("Edad del jugador: " + edad);
+
+        // Escribe una condición:
+        // Recuerda: lo que está dentro de los paréntesis en if() es la condición. Si se cumple, se ejecutará el código encapsulado en llaves.
+        // Si no se cumple, se ejecutará el código encapsulado en else
+        
+        // El bucle for sirve para repetir acciones hasta cumplir una condición.
+        // for ( <tipo_variable> <nombre_variable> = <valor_asignado>; <nombre_variable> <operador> <valor>; <nombre_variable> <operador ++ o -->
+        // Ejemplo: 
+        /** for (int energia = 1; energia <= 5; energia++) {
+            System.out.println("Cargando escudo... Energía al " + (energia * 20) + "%");
+        }**/
+    }
+}
