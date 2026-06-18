bbc6cd098852961e80405b27a1067d3ae1208334
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 15:07:53 -0500

    docs: add starter file for lesson 6

---
+++ starter-files/lesson-6-errors-and-practices/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void saludar() {
+        System.out.println("Hola estudiante");
+    }
+
+    public static void mostrarBienvenida(String nombre) {
+        System.out.println("Hola " + nombre);
+    }
+
+    public static void main(String[] args) {
+        // Identifica los errores en este código y aplica buenas prácticas donde corresponda.
+        int edad = 15
+        System.out.println("Edad: " + edad);
+
+        String nombre = "Ana";
+      
+        System.out.println(Nombre);
+
+        int edadComparacion = 18;
+
+        if (edadComparacion = 18) {
+        System.out.println("Tienes 18 años");
+        }
+
+
+        int contador = 0;
+
+        while(contador < 5) {
+        System.out.println(contador);
+        }
+        
+        String a = "Carlos";
+        int b = 15;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        if (edadEstudiante >= 12) {
+        System.out.println("Bienvenido");
+        }
+
+        saludar();
+
+
+    }
+}
