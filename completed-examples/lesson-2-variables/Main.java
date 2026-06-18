b41acbaf321d3ff762dc38500c23f74c446a4cb0
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 13:05:41 -0500

    docs: add completed example 2 java file

---
+++ completed-examples/lesson-2-variables/Main.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Andrea";
+        int age = 19;
+        double height = 1.65;
+        char grade = 'A';
+        boolean student = true;
+
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+        System.out.println("Altura: " + height);
+        System.out.println("Nota: " + grade);
+        System.out.println("¿Es estudiante?: " + student);
+    }
+}
