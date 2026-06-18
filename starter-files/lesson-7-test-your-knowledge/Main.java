f26b80102991b764bc97ec66b2d897f098185b63
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-06-18 15:10:49 -0500

    docs: add starter file for lesson 7
    Implement Producto class with attributes and constructor.

---
+++ starter-files/lesson-7-test-your-knowledge/Main.java
@@ -0,0 +1,19 @@
+class Producto {
+    private String nombre;
+    private int cantidad;
+    private double precio;
+    
+    public Producto(String nombre, int cantidad, double precio){
+      //Completar
+    }
+}
+
+public class Main {
+    public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+
+        //Crea una clase producto con los siguientes atributos: nombre, cantidad y precio, y asegúrate de que se muestren los
+        //valores registrados después de crear un objeto de dicha clase. Estos atributos deben poder ser ingresados por el
+        //usuario y deben ser validados.
+    }
+}
